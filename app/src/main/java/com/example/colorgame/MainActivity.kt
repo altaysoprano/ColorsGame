7252e714c0ac4add25b75d2a122e58b614698a1a
Hasan Cihad Altay <caltayzzz@gmail.com>
2023-07-03 18:12:48 +0300

    authomatic color changing in every 5 seconds

--- app/src/main/java/com/example/colorgame/MainActivity.kt
+++ app/src/main/java/com/example/colorgame/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.colorgame
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
@@ -7,28 +8,64 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.example.colorgame.databinding.ActivityMainBinding
 import com.skydoves.colorpickerview.ColorPickerView
 import com.skydoves.colorpickerview.listeners.ColorListener
+import java.util.Random
+import java.util.Timer
+import java.util.TimerTask
 
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
+    private val random = Random()
+    private val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val colorPickerView = findViewById<ColorPickerView>(R.id.colorPickerView)
-        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         viewModel.selectedColor.observe(this, Observer { color ->
-            linearLayout.setBackgroundColor(color)
+            binding.linearLayout.setBackgroundColor(color)
         })
 
-        colorPickerView.setColorListener(ColorListener { color, fromUser ->
-            viewModel.onColorChanged(color)
+        binding.colorPickerView.setColorListener(ColorListener { color, fromUser ->
+            binding.linearLayout.setBackgroundColor(color)
         })
 
+        startColorChangeTimer()
     }
-}
+
+    private fun startColorChangeTimer() {
+        val delay = 0L
+        val period = 5000L // 5 saniye
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val randomColor = getRandomColor()
+                runOnUiThread {
+                    binding.dynamicLayout.setBackgroundColor(randomColor)
+                }
+            }
+        }, delay, period)
+    }
+
+    private fun getRandomColor(): Int {
+        val alpha = 255 // Opağı tam olarak ayarla (0-255 aralığında)
+        val red = random.nextInt(256)
+        val green = random.nextInt(256)
+        val blue = random.nextInt(256)
+
+        return Color.argb(alpha, red, green, blue)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+    }
+    }
